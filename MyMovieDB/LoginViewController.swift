24e994d31e585dc18445a839f6db2b4b5dda5172
dianagreyes <dianagreyes29@gmail.com>
2022-09-22 01:10:49 -0500

    Login without services

---
+++ MyMovieDB/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  MyMovieDB
+//
+//  Created by Giselle RS on 20/09/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        if (self.usernameTextField.text == "admin" && self.passwordTextField.text == "12345678a") {
+        }
+        else {
+        }
+    }
+    
+    sklmds
+    sjndsx
+    xmas xsa
+    
+    
+    
+}
